35d5a9d9c663d1ed2b669dc9030a4e3d6c6fdfd4
Frans Hoekstra <frans.hoekstra@iddinkgroup.com>
2021-12-04 22:00:40 +0100

    some day4 changes

--- go/day4/main.go
+++ go/day4/main.go
@@ -15,17 +15,25 @@ type board struct {
 
 func (b *board) hasbingo() bool {
 	for counter := 0; counter < 5; counter++ {
-		if b.numbers[counter][0] == -1 && b.numbers[counter][1] == -1 && b.numbers[counter][2] == -1 && b.numbers[counter][3] == -1 && b.numbers[counter][4] == -1 {
-			return true
+		xok, yok := true, true
+		for counter2 := 0; counter2 < 5; counter2++ {
+			if b.numbers[counter][counter2] > -1 {
+				xok = false
+			}
+			if b.numbers[counter2][counter] > -1 {
+				yok = false
+			}
 		}
-		if b.numbers[0][counter] == -1 && b.numbers[1][counter] == -1 && b.numbers[2][counter] == -1 && b.numbers[3][counter] == -1 && b.numbers[4][counter] == -1 {
+		if xok || yok {
 			return true
 		}
 	}
 	return false
 }
 
-func (b *board) play(draw int) {
+func (b *board) play(draw int) (bingo bool) {
+	b.runs += 1
+	b.last = draw
 	for counter := 0; counter < 5; counter++ {
 		for counter2 := 0; counter2 < 5; counter2++ {
 			if b.numbers[counter][counter2] == draw {
@@ -34,8 +42,7 @@ func (b *board) play(draw int) {
 			}
 		}
 	}
-	b.runs += 1
-	b.last = draw
+	return b.hasbingo()
 }
 
 func main() {
@@ -53,14 +60,13 @@ func main() {
 			bingocard := board{}
 			for counter := 0; counter < 5; counter++ {
 				for counter2 := 0; counter2 < 5; counter2++ {
-					nr, _ := strconv.Atoi(strings.Trim((input[i+1+counter][counter2*3 : (counter2*3)+2]), " "))
+					nr, _ := strconv.Atoi(strings.Trim(input[i+1+counter][counter2*3:(counter2*3)+2], " "))
 					bingocard.numbers[counter][counter2] = nr
 					bingocard.remainer += nr
 				}
 			}
 			for _, draw := range draws {
-				bingocard.play(draw)
-				if bingocard.hasbingo() {
+				if bingocard.play(draw) {
 					boards = append(boards, bingocard)
 					break
 				}
