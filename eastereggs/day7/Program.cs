6042876bac4c18b3f08c11a5f6c23d42664d50b2
Frans Hoekstra <frans.hoekstra@iddinkgroup.com>
2021-12-17 17:49:14 +0100

    day7 easter egg

---
+++ eastereggs/day7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace aoc2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            intcode.Go();
+            watch.Stop();
+
+            Console.WriteLine("Time taken: {0} ms", watch.ElapsedMilliseconds);
+        }
+    }
+}
